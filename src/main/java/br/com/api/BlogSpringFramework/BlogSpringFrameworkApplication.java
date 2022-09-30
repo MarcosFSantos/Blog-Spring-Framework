3b139744c5b24b1a102f6c9b1dddf006864f80a7
Marcos Vinícius <marcos.franca1673@gmail.com>
2022-09-30 15:36:13 -0300

    FEAT: Generating the project with Spring Initializr

---
+++ src/main/java/br/com/api/BlogSpringFramework/BlogSpringFrameworkApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.BlogSpringFramework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSpringFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogSpringFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/BlogSpringFramework/BlogSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.BlogSpringFramework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
