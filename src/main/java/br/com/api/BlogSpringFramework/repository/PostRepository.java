6008e7efdb8a9d65d949992be5942798b5c09755
Marcos Vinícius <marcos.franca1673@gmail.com>
2022-10-03 13:58:48 -0300

    FEAT: Updating the Post repository, adding the findAll() abstract method

--- src/main/java/br/com/api/BlogSpringFramework/repository/PostRepository.java
+++ src/main/java/br/com/api/BlogSpringFramework/repository/PostRepository.java
@@ -3,5 +3,17 @@ package br.com.api.BlogSpringFramework.repository;
 import br.com.api.BlogSpringFramework.model.Post;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PostRepository extends CrudRepository<Post, Integer> {
+
+    //Criar e/ou atualizar uma postagem no banco de dados.
+
+    //Ler todos as postagens do banco de dados.
+    List<Post> findAll();
+
+    //Ler uma postagem específica do banco de dados.
+
+    //Deletar uma postagem do banco de dados.
+
 }
